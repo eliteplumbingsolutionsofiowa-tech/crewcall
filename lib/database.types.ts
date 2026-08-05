7ea20c91c03a68a301f33211c772b78c4c4df32a
Jay Gohn <jaygohn@Mac.home.local>
2026-08-04 20:36:51 -0500

    Update Supabase database types

--- lib/database.types.ts
+++ lib/database.types.ts
@@ -250,6 +250,36 @@ export type Database = {
           },
         ]
       }
+      device_tokens: {
+        Row: {
+          created_at: string
+          device_name: string | null
+          id: string
+          platform: string
+          token: string
+          updated_at: string
+          user_id: string
+        }
+        Insert: {
+          created_at?: string
+          device_name?: string | null
+          id?: string
+          platform: string
+          token: string
+          updated_at?: string
+          user_id: string
+        }
+        Update: {
+          created_at?: string
+          device_name?: string | null
+          id?: string
+          platform?: string
+          token?: string
+          updated_at?: string
+          user_id?: string
+        }
+        Relationships: []
+      }
       invites: {
         Row: {
           accepted: boolean | null
